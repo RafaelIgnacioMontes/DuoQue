07a5781bd7f14e636dcdface16e32254f4eedf14
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-04-04 11:44:48 -0600

    Fixed import on ProfilePage

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -6,7 +6,7 @@ import silver from '../images/tier-icons/silver_iii.png'
 import bronze from '../images/tier-icons/bronze_iii.png'
 import diamond from '../images/tier-icons/diamond_iii.png'
 import plat from '../images/tier-icons/platinum_iii.png'
-import UpdatProfile from '../components/UpdateProfileInfo'
+import UpdateProfile from '../components/UpdateProfileInfo'
 
 const ProfilePage = ({ summoner, GetSummonerProfile, summonerProfile }) => {
   const [rankInfo, setRankInfo] = useState({
